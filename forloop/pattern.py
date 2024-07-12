f107d0bc562e9b8e5320398cc51d967d1de41a64
Vishnu Maurya <mauryavishnu98@gmail.com>
2024-07-12 21:23:12 +0530

    new patterns

--- forloop/pattern.py
+++ forloop/pattern.py
@@ -1,5 +1,117 @@
 # n = int(input("Number of rows : "))
 
+for i in range(1, 5 + 1):
+    for j in range(5, i, -1):
+        print(" ", end=" ")
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+for i in range(4, 0, -1):
+    for j in range(5, i, -1):
+        print(" ", end=" ")
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+#         1
+#       2 1
+#     3 2 1
+#   4 3 2 1
+# 5 4 3 2 1
+#   4 3 2 1
+#     3 2 1
+#       2 1
+#         1
+
+# for i in range(1, 5 + 1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+# for i in range(4, 0, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+#         1
+#       1 2
+#     1 2 3
+#   1 2 3 4
+# 1 2 3 4 5
+#   1 2 3 4
+#     1 2 3
+#       1 2
+#         1
+
+
+# for i in range(1, 5 + 1):
+#     for j in range(5, 0, -1):
+#         if j > i:
+#             print(" ", end=" ")
+#         else:
+#             print("*", end=" ")
+#     print()
+# for i in range(4, 0, -1):
+#     for j in range(5, 0, -1):
+#         if j > i:
+#             print(" ", end=" ")
+#         else:
+#             print("*", end=" ")
+#     print()
+
+
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+
+# for i in range(1, (n // 2) + 1 + 1):
+#     for j in range(5, i):
+#         print(j, end=" ")
+#     print()
+# for i in range(n // 2, 1, -1):
+#     for i in range(1, i):
+#         print(j, end=" ")
+#     print()
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 5 5 5 5
+# 5 5 5
+# 5 5
+# 5
+
+
+# for i in range(1, (n // 2) + 1 + 1):
+#     for j in range(1, i):
+#         print("*", end=" ")
+#     print()
+# for i in range(n // 2, 1, -1):
+#     for j in range(1, i):
+#         print("*", end=" ")
+#     print()
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
 # for i in range(1, 5 + 1):
 #     for j in range(1, 5 - i + 1):
 #         print(" ", end=" ")
