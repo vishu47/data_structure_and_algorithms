78f7180edf2c558226f05d77e8c162cd331d4997
Vishnu <mauryavishnu98@gmail.com>
2025-05-16 20:41:29 +0530

    kadansealgo

---
+++ rev1/gfg/KadanesAlgorithm.py
@@ -0,0 +1,38 @@
+# Maximum Subarray Sum – Kadane's Algorithm
+# loop and sum if sum is less than 0 then sum replace with 0 and move forword 
+def KadanesAlgorithm():
+    arr = [2, 3, -8, 7, -1, 2, 3]
+    # msum = float("-inf")
+    # sum = 0
+    # for i in range(0, len(arr)):
+    #     for j in range(i, len(arr)):
+    #         sum = sum + arr[j]
+    #         msum = max(msum , sum)
+            
+    #     sum = 0
+    # print(msum)
+    
+    msum = float("-inf")
+    sum = 0
+    start = -1
+    st , en = -1, -1
+    for i in range(0 , len(arr)):
+        # whenever sum 0 it will start from i 
+        if sum == 0 : start = i
+        
+        sum = sum + arr[i]
+        
+        if sum > msum:
+            msum = sum
+            st = start 
+            ed = i + 1
+        
+        if sum < 0: 
+            sum = 0
+            
+    print(st , ed, msum)
+    return arr[st:ed]
+    
+    
+print(KadanesAlgorithm())
+ 
--- rev1/gfg/majorityElement.py
+++ rev1/gfg/majorityElement.py
@@ -1,15 +1,41 @@
 def majorityElement():
-    arr = [7]
-    mp = {}
-    for i in arr:
-        if i in mp:
-           mp[i] = mp[i] + 1
+    arr = [4,4,2,4,3,4,4,3,2,4]
+    # arr = [2,2,1,1,1,2,2]
+    # mp = {}
+    # for i in arr:
+    #     if i in mp:
+    #        mp[i] = mp[i] + 1
+    #     else:
+    #         mp[i] = 1
+    # n = len(arr)//2
+    # f = -1
+    # for [key , val] in mp.items():
+    #     if val > n:
+    #         return key
+    
+    
+    # moore voting algo
+    m = 0
+    c = 0
+    for i in range(0 , len(arr)):
+        if c == 0:
+            m = arr[i]
+            
+        if m == arr[i]:
+            c+=1
         else:
-            mp[i] = 1
-    n = len(arr)//2
-    f = -1
-    for [key , val] in mp.items():
-        if val > n:
-            return key
+            c-=1
+    n = len(arr)//2 
+    
+    count = 0
+    for i in arr:
+        if i == m:
+            count+=1 
+                   
+    if count > n:
+        return m 
+    return -1       
+    
+    print(m, count)
     
 print(majorityElement())
